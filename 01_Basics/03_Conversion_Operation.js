aee710eaef725c55982313f24c9fe38ab935b50f
gayatribidwai <133579551+gayatribidwai@users.noreply.github.com>
2024-06-24 10:06:50 +0000

    Conversion and Operations.

--- 01_Basics/03_Conversion_Operation.js
+++ 01_Basics/03_Conversion_Operation.js
@@ -6,8 +6,8 @@ let sco=undefined
 let scor=true
 let ss="Gayatri"
 
-console.log(typeof score); //o/p - string
-console.log(typeof(score)); //() - as a method o/p - string
+// console.log(typeof score); //o/p - string
+// console.log(typeof(score)); //() - as a method o/p - string
 
 let valueInNumber = Number(score)
 let valuesc = Number(sc)
@@ -30,12 +30,49 @@ console.log(valuess); //o/p - NaN
 
 let isLoggedIn="Gayu"
 let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn) // o/p=> true
+//console.log(booleanIsLoggedIn) // o/p=> true
+
 // 1 => true; 0 => false
 // "" => false
 // "Gayu" => true
 
 let somenumber =33
 let stringNumber=String(somenumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// **************************************** Operations **********************************
+
+ let value =3
+ let negValue=-value
+//  console.log(negValue);
+//  console.log(2*2);// 4
+//  console.log(2**3);// 2 to the power 3 = 8
+
+let str1="Hello"
+let str2=" Gayatri"
+let str3=str1+str2
+//console.log(str3);
+
+// console.log("1"+2); // 12
+// console.log(1+"2"); // 12
+// console.log("1"+"2"); // 12
+// console.log("1"+2+2); // 122
+// console.log(1+2+"2"); // 32
+//Output Produces according to Ecma Script Guidelines. //research topic for creating browser engine and all.😶
+
+//console.log( (3+4) * 5 % 3); // Review your code.
+
+// console.log(+true); //o/p => 1
+// //console.log(true+); //o/p => Error
+//console.log(+""); //o/p => 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter =100
+++gameCounter
+console.log(gameCounter); //learn prefix, postfix 
+
+// Learn anything on Ecma something.😅 
