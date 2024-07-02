015ec7d0c999c4c6792bf423a766128ac5947a9d
gayatribidwai <133579551+gayatribidwai@users.noreply.github.com>
2024-07-02 13:58:39 +0000

    datatype- summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+//primitive - call by value - copied reference is used and changes made on copy not actual data.
+// 7 types -> String, Number, Boolean, null, undefined, Symbol(to make any value unique), Bigint
+// 
+
+const score = 100
+const isLoggedin = false
+const outsideTemp = null
+let userEmail; // default value undefined.
+
+const id = Symbol('123')
+const anotheId = Symbol('123')
+
+console.log(id === anotheId); //symbol always give unique values in case of same data also.
+
+const bigNumber = 56835264940372648n // automatically become bigint (n at the end)
+
+
+//reference type (nonprimitive)
+// Array, Objects, Functions.
+
+
+const heros = ["Shaktiman", "nagraj", "doga"];
+
+//Object
+let myObj = {
+    name: "Gayatri",
+    age: 22
+}
+
+// Function as a variable
+
+const myFunction = function()
+{
+    console.log("Hello World!");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof myFunction); // function - some documentation (function object)
+console.log(typeof heros); //Object
